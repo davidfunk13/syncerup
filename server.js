5abd5478b31898b344805649aedcc0fbbb380191
davidfunk13 <davefunk135@gmail.com>
2020-11-26 02:42:39 -0800

    made api live message behind cathc all

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 const server = require('http').createServer(app);
 
-const PORT = process.env.PORT | 3001;
+const PORT = process.env.PORT || 3001;
 
 const cors = require('cors');
 
