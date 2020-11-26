687e77446dc284aab098c5321684fca8e90c7403
davidfunk13 <davefunk135@gmail.com>
2020-11-25 18:10:18 -0800

    cors

--- server.js
+++ server.js
@@ -4,17 +4,19 @@ const app = express();
 
 const server = require('http').createServer(app);
 
+const cors = require('cors');
+
 const io = require('socket.io')(server);
 
 const PORT = process.env.PORT || 3001;
+
+app.use(cors());
 
 app.use(express.json());
 
 io.on('connection', socket => {
     socket.emit('message', "connected")
 });
-
-
 
 server.listen(PORT, () => {
     console.log(`Application server listening on port ${PORT}`);
