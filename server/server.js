f416026c1083985e6ec41ee0dad2eac145f795b4
Dave <davefunk135@gmail.com>
2020-11-30 20:42:49 -0800

    hosting frontend here as well because it makes the most sense for what i need from this project.

--- server/server.js
+++ server/server.js
@@ -4,11 +4,6 @@ const server = require('http').createServer(app);
 
 const PORT = process.env.PORT || 3001;
 
-const corsOptions = {
-    origin: [`https://davidfunk13.github.io/`, `http://localhost:3001`],
-    optionsSuccessStatus: 200,
-};
-
 app.use(require('express').json());
 
 const options = {
@@ -20,6 +15,7 @@ const options = {
 const io = require('socket.io')(server, options);
 
 function recieveMessage(socket, message) {
+    console.log(socket, message)
     socket.emit('notification', { message: 'message recieved', data: message })
 }
 
@@ -28,21 +24,22 @@ io.on('connection', socket => {
 
     socket.on('message', data => recieveMessage(socket, data));
 
-    // socket.on('joinRoom', ({ username, room }, () => console.log('fart')))
-    socket.on('joinRoom', ({ username, room }, callback) => {
-        recieveMessage(socket, { username, room });
-        // callback();
-    })
+    socket.on('joinRoom', ({ username, room }) => recieveMessage(socket, ({ username, room })));
 
     socket.on('disconnect', () => {
         console.log('disconnected', socket.id);
     })
 });
 
+if (process.env.NODE_ENV === 'production') {
+    //if you have weird errors with storing images etc in static dir this is why. back out a dir.
+    app.use(express.static("client/build"));
+
+    app.get('/*', function (req, res) {
+        res.sendFile(path.join(__dirname, './client/build/index.html'));
+    });
+};
+
 server.listen(PORT, () => {
     console.log('Server listening on port ' + PORT)
 });
-
-app.get('/*', (req, res) => {
-    res.json({ message: 'api is live' });
-})
